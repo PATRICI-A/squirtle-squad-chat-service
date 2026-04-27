44cf4eb0e7515af644951c14b3e12c9c5225e28c
CristianGuerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-04-27 12:56:06 -0500

    Feat: Configuracion Maven

---
+++ src/test/java/com/patricia/chat/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patricia.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

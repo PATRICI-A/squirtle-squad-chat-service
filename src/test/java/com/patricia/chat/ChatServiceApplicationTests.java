a1641b994ba16bf872974fce67abdca458aefc9e
ders14028-bit <ders14028@gmail.com>
2026-05-21 23:46:02 -0500

    ajustes

--- src/test/java/com/patricia/chat/ChatServiceApplicationTests.java
+++ src/test/java/com/patricia/chat/ChatServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.patricia.chat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ChatServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
 
+    @Test
+    void contextLoads() {
+    }
 }
